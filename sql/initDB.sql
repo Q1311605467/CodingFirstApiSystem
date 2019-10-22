c4665c5686409532ca6878aa9dbf361829f61c8a
axiang <wyxworkmail@163.com>
2019-10-22 11:08:50 +0800

    feat(数据库): 初始化数据库
    - 编写数据库初始化脚本

--- sql/initDB.sql
+++ sql/initDB.sql
@@ -1,50 +1,486 @@
+# DROP DATABASE IF EXISTS coding_first;
+
 CREATE DATABASE coding_first;
 
-DROP TABLE IF EXISTS t_user_base_info;
-CREATE TABLE t_user_base_info
+USE coding_first;
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 系统信息表
+DROP TABLE IF EXISTS `t_system_info`;
+CREATE TABLE `t_system_info`
+(
+    id    INT PRIMARY KEY AUTO_INCREMENT,
+    name  VARCHAR(255) NOT NULL COMMENT '属性名',
+    value TEXT         NOT NULL COMMENT '属性值'
+);
+
+# 系统日志表
+DROP TABLE IF EXISTS `t_system_log`;
+CREATE TABLE `t_system_log`
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    level      VARCHAR(10) DEFAULT 'default'
+        COMMENT '日志等级：[default, debug, info, warning, error, fatal]',
+    info       TEXT NOT NULL,
+    time       DATETIME    DEFAULT NOW(),
+    username   VARCHAR(30) COMMENT '产生日志的用户名',
+    ip_address VARCHAR(30) COMMENT '产生日志的地址',
+    INDEX (level)
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# FJUT荣誉榜单展示表
+DROP TABLE IF EXISTS `t_border_honor_rank`;
+CREATE TABLE `t_border_honor_rank`
 (
     id              INT PRIMARY KEY AUTO_INCREMENT,
-    username        VARCHAR(16) UNIQUE COMMENT '16个英文字母的登录名',
+    username_one    VARCHAR(30),
+    real_name_one   VARCHAR(10) COMMENT '真实姓名',
+    username_two    VARCHAR(30),
+    real_name_two   VARCHAR(10),
+    username_three  VARCHAR(30),
+    real_name_three VARCHAR(10),
+    reward_date     DATE,
+    register_time   DATETIME DEFAULT NOW() COMMENT '登记该条记录的时间',
+    contest_level   INT COMMENT '比赛等级',
+    award_level     INT COMMENT '获奖等级',
+    description     TEXT COMMENT '获奖描述'
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 用户基本信息表
+DROP TABLE IF EXISTS `t_user_base_info`;
+CREATE TABLE `t_user_base_info`
+(
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    username        VARCHAR(16) NOT NULL UNIQUE COMMENT '16个英文字母的登录名',
     nick            VARCHAR(8)  NOT NULL COMMENT '八个汉字或者八个字符',
     gender          SMALLINT COMMENT '性别，0为保密，1为男，2为女',
     email           VARCHAR(50) NOT NULL COMMENT '邮箱',
     phone           VARCHAR(20) NOT NULL COMMENT '电话',
     motto           VARCHAR(50) COMMENT '个性签名',
-    register_time   DATETIME    NOT NULL COMMENT '注册时间',
+    register_time   DATETIME    NOT NULL DEFAULT NOW() COMMENT '注册时间',
     rating          INT         NOT NULL DEFAULT 0 COMMENT '评分',
-    ranking         INT                  DEFAULT 0 COMMENT '排行',
-    ac_num          INT                  DEFAULT 0 COMMENT 'AC题数',
+    ranking         INT         NOT NULL DEFAULT 0 COMMENT '排行',
+    ac_num          INT         NOT NULL DEFAULT 0 COMMENT 'AC题数',
     school          VARCHAR(20) COMMENT '学校',
     faculty         VARCHAR(20) COMMENT '学院',
     major           VARCHAR(20) COMMENT '专业',
     cla             VARCHAR(20) COMMENT '班级',
-    student_id      VARCHAR(20)          DEFAULT NULL COMMENT '学号',
-    graduation_time DATETIME             DEFAULT NULL COMMENT '毕业时间'
+    student_id      VARCHAR(20) COMMENT '学号',
+    graduation_year VARCHAR(10) COMMENT '毕业年份，字符串类型'
 );
 
+# 用户登录授权表
 DROP TABLE IF EXISTS `t_user_auth`;
 CREATE TABLE `t_user_auth`
 (
     id                       INT PRIMARY KEY AUTO_INCREMENT,
-    username                 varchar(30) NOT NULL UNIQUE,
-    salt                     char(32)    NOT NULL COMMENT '加密盐值',
-    password                 char(40)    NOT NULL,
-    attempt_login_fail_count smallint(6) DEFAULT '0' COMMENT '尝试登录失败次数',
-    locked                   smallint(6) DEFAULT '0' COMMENT '0为未锁定，1为锁定',
-    unlock_time              datetime    DEFAULT NULL COMMENT '账号解锁时间',
-    last_login_time          datetime    DEFAULT NULL,
+    username                 VARCHAR(30) NOT NULL UNIQUE,
+    salt                     CHAR(32)    NOT NULL COMMENT '加密盐值',
+    password                 CHAR(40)    NOT NULL,
+    attempt_login_fail_count SMALLINT DEFAULT 0 COMMENT '尝试登录失败次数',
+    locked                   SMALLINT DEFAULT 0 COMMENT '0为未锁定，1为锁定',
+    unlock_time              DATETIME COMMENT '账号解锁时间',
+    last_login_time          DATETIME,
     FOREIGN KEY (username) REFERENCES t_user_base_info (username)
 );
 
+# 用户个性化信息表
 DROP TABLE IF EXISTS `t_user_custom_info`;
 CREATE TABLE `t_user_custom_info`
 (
-    id         INT PRIMARY KEY AUTO_INCREMENT,
-    username   VARCHAR(30) NOT NULL UNIQUE,
-    avatar_url VARCHAR(255) COMMENT '用户头像url地址',
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    username        VARCHAR(30) NOT NULL UNIQUE,
+    avatar_url      VARCHAR(255) COMMENT '用户头像url地址',
     adjective_title VARCHAR(10) COMMENT '形容词头衔',
-    article_title VARCHAR(10) COMMENT '名词头衔',
+    article_title   VARCHAR(10) COMMENT '名词头衔',
     FOREIGN KEY (username) REFERENCES t_user_base_info (username)
 );
 
+# 用户头衔表
+DROP TABLE IF EXISTS `t_user_title`;
+CREATE TABLE `t_user_title`
+(
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    username        VARCHAR(30) NOT NULL UNIQUE,
+    type            INT COMMENT '头衔类型：0：未定义；1：形容词；2：名词头衔',
+    name            VARCHAR(10) NOT NULL COMMENT '头衔名称',
+    picture_url     VARCHAR(2047) COMMENT '头衔图片url',
+    validity_period INT DEFAULT -1 COMMENT '头衔有效期，单位：天，-1为永久',
+    FOREIGN KEY (username) REFERENCES t_user_base_info (username)
+);
 
+# 用户印章表
+DROP TABLE IF EXISTS `t_user_seal`;
+CREATE TABLE `t_user_seal`
+(
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    username        VARCHAR(30)  NOT NULL UNIQUE,
+    name            VARCHAR(255) NOT NULL COMMENT '印章名称',
+    picture_url     VARCHAR(2047) COMMENT '印章图片url',
+    validity_period INT DEFAULT -1 COMMENT '印章有效期，单位：天，-1为永久',
+    FOREIGN KEY (username) REFERENCES t_user_base_info (username)
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 系统权限类型表
+DROP TABLE IF EXISTS `t_permission_type`;
+CREATE TABLE `t_permission_type`
+(
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    permission_name VARCHAR(20) NOT NULL
+);
+
+# 用户系统权限表
+DROP TABLE IF EXISTS `t_user_permission`;
+CREATE TABLE `t_user_permission`
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    username      VARCHAR(30) NOT NULL,
+    permission_id INT         NOT NULL,
+    granter       VARCHAR(30) COMMENT '授权人',
+    grant_time    DATETIME DEFAULT NOW() COMMENT '授予时间',
+    FOREIGN KEY (username) REFERENCES t_user_base_info (username)
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 题目基础信息表
+DROP TABLE IF EXISTS `t_problem_info`;
+CREATE TABLE `t_problem_info`
+(
+    id                INT PRIMARY KEY AUTO_INCREMENT,
+    problem_id        INT UNIQUE COMMENT '题目的题号',
+    title             VARCHAR(80) NOT NULL COMMENT '题目标题',
+    belong_oj_id      INT         NOT NULL DEFAULT 0 COMMENT '题目来源哪个OJ, 0为本地',
+    belong_problem_id VARCHAR(10) COMMENT '题目在来源OJ里的ID',
+    author            VARCHAR(30)          DEFAULT 'spider' COMMENT '出题人',
+    total_submit      INT         NOT NULL DEFAULT 0 COMMENT '总共提交次数',
+    total_ac          INT         NOT NULL DEFAULT 0 COMMENT '提交次数内总共AC次数',
+    total_submit_user INT         NOT NULL DEFAULT 0 COMMENT '总共提交用户人数',
+    total_ac_user     INT         NOT NULL DEFAULT 0 COMMENT '提交用户内总共AC人数',
+    visible           TINYINT(1)           DEFAULT 1 COMMENT '是否普通用户可见，0： 不可见， 1：可见',
+    judge_option      SMALLINT COMMENT '评测机制选择： 0：默认，1：本地评测优先；2：第三方评测优先',
+    INDEX (problem_id),
+    INDEX (belong_oj_id)
+);
+
+# 题目详情表
+DROP TABLE IF EXISTS `t_problem_view`;
+CREATE TABLE `t_problem_view`
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    problem_id   INT UNIQUE,
+    time_limit   VARCHAR(30) NOT NULL COMMENT '时间限制',
+    memory_limit VARCHAR(30) NOT NULL COMMENT '内存限制',
+    int_format   VARCHAR(10) NOT NULL COMMENT '64位int使用的格式化字符串',
+    spj          TINYINT(1)  NOT NULL COMMENT '特判标记, 0：不是，1：是',
+    description  TEXT        NOT NULL COMMENT '题目描述',
+    input        TEXT        NOT NULL COMMENT '输入',
+    output       TEXT        NOT NULL COMMENT '输出',
+    FOREIGN KEY (problem_id) REFERENCES t_problem_info (problem_id)
+);
+
+# 题目样例表
+DROP TABLE IF EXISTS `t_problem_sample`;
+CREATE TABLE `t_problem_sample`
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    problem_id  INT UNIQUE,
+    input_case  TEXT NOT NULL COMMENT '输入样例',
+    output_case TEXT NOT NULL COMMENT '输出样例',
+    FOREIGN KEY (problem_id) REFERENCES t_problem_info (problem_id)
+);
+
+# 题目难度表
+DROP TABLE IF EXISTS `t_problem_difficult`;
+CREATE TABLE `t_problem_difficult`
+(
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    problem_id      INT UNIQUE,
+    difficult_level SMALLINT DEFAULT NULL COMMENT '难度级别 1~3 ，越高越难',
+    FOREIGN KEY (problem_id) REFERENCES t_problem_info (problem_id)
+);
+
+# 题目标签表
+DROP TABLE IF EXISTS `t_problem_tag`;
+CREATE TABLE `t_problem_tag`
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(10) NOT NULL COMMENT '标签名称',
+    tag_type    INT         DEFAULT -1 COMMENT '标签隶属类型',
+    create_user VARCHAR(30) DEFAULT 'SYSTEM' COMMENT '标签创建者',
+    priority    INT         DEFAULT 0 COMMENT '优先级'
+);
+
+# 题目标签记录表
+DROP TABLE IF EXISTS `t_problem_tag_record`;
+CREATE TABLE `t_problem_tag_record`
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    username   VARCHAR(30) NOT NULL,
+    tag_id     INT         NOT NULL,
+    time       DATETIME DEFAULT NOW() COMMENT '贴标签时间',
+    confidence DOUBLE   DEFAULT 0 COMMENT '标签置信度'
+);
+
+# 题目收藏表
+DROP TABLE IF EXISTS `t_problem_star`;
+CREATE TABLE `t_problem_star`
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    problem_id INT         NOT NULL COMMENT '收藏的题目ID',
+    username   VARCHAR(30) NOT NULL,
+    time       DATETIME     DEFAULT NOW() COMMENT '收藏时间',
+    mark       VARCHAR(255) DEFAULT '' COMMENT '备注'
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 比赛基本信息表
+DROP TABLE IF EXISTS `t_contest_info`;
+CREATE TABLE `t_contest_info`
+(
+    id                  INT PRIMARY KEY AUTO_INCREMENT,
+    contest_id          INT UNIQUE COMMENT '比赛编号',
+    contest_kind        INT COMMENT '比赛类型： 0：练习；1：积分；2：趣味；3：正式；4：隐藏；5：DIY',
+    title               VARCHAR(30) NOT NULL COMMENT '比赛标题',
+    description         TEXT        NOT NULL COMMENT '比赛介绍',
+    create_user         VARCHAR(30) COMMENT '比赛开设人',
+    create_time         DATETIME             DEFAULT NOW() COMMENT '比赛创建时间',
+    begin_time          DATETIME    NOT NULL COMMENT '比赛开始时间',
+    end_time            DATETIME    NOT NULL COMMENT '比赛结束时间',
+    permission_type     INT         NOT NULL DEFAULT 0
+        COMMENT '权限类型： 0：公开；1：密码；2：私有；3：注册；4：正式；5：组队',
+    password            VARCHAR(16) COMMENT '需要密码时的密码',
+    register_begin_time DATETIME COMMENT '需要注册参赛信息时的注册开始时间',
+    register_end_time   DATETIME COMMENT '需要注册参赛信息时的注册结束时间',
+    computer_rating     TINYINT(1)           DEFAULT 0 COMMENT '是否计算积分',
+    rank_type           INT         NOT NULL COMMENT '计分方式',
+    problem_put_tag     TINYINT(1)           DEFAULT 1
+        COMMENT '题目是否可以贴标签： 0：不可以，1：可以，定义同下',
+    status_read_out     TINYINT(1)           DEFAULT 1 COMMENT '是否可以查看评测列表',
+    show_register_list  TINYINT(1)           DEFAULT 1 COMMENT '展示注册名单',
+    show_border_list    TINYINT(1)           DEFAULT 1 COMMENT '展示比赛版单',
+    show_other_status   TINYINT(1)           DEFAULT 1 COMMENT '展示别人的代码',
+    INDEX (create_user)
+);
+
+# 比赛题目表
+DROP TABLE IF EXISTS `t_contest_problem`;
+CREATE TABLE `t_contest_problem`
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    contest_id    INT COMMENT '比赛编号',
+    problem_id    INT COMMENT '题目的题号',
+    problem_order INT COMMENT '题目在比赛内的顺序',
+    FOREIGN KEY (contest_id) REFERENCES t_contest_info (contest_id),
+    FOREIGN KEY (problem_id) REFERENCES t_problem_info (problem_id)
+);
+
+# 比赛注册成员表
+DROP TABLE IF EXISTS `t_contest_register_user`;
+CREATE TABLE `t_contest_register_user`
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    contest_id    INT         NOT NULL,
+    username      VARCHAR(30) NOT NULL,
+    register_time DATETIME DEFAULT NOW() COMMENT '注册时间',
+    review_status SMALLINT DEFAULT 0 COMMENT '审核是否通过: 0:还未审核；1：审核通过；2：审核失败',
+    review_info   VARCHAR(255) COMMENT '审核反馈信息',
+    review_time   DATETIME COMMENT '审核时间',
+    FOREIGN KEY (contest_id) REFERENCES t_contest_info (contest_id),
+    INDEX (username)
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 评测信息表
+DROP TABLE IF EXISTS `t_judge_status`;
+CREATE TABLE `t_judge_status`
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    username    VARCHAR(30) NOT NULL,
+    problem_id  INT         NOT NULL,
+    contest_id  INT         NOT NULL DEFAULT -1,
+    language    INT         NOT NULL COMMENT '使用语言',
+    submit_time DATETIME             DEFAULT NOW() COMMENT '提交时间',
+    result      INT         NOT NULL COMMENT '评测结果',
+    score       INT COMMENT '如果是spj类型的题目，这里显示得分',
+    time_used   VARCHAR(10) NOT NULL COMMENT '评测耗时',
+    memory_used VARCHAR(10) NOT NULL COMMENT '内存消耗',
+    code        TEXT        NOT NULL COMMENT '评测代码',
+    code_length INT         NOT NULL COMMENT '代码长度',
+    FOREIGN KEY (problem_id) REFERENCES t_problem_info (problem_id),
+    FOREIGN KEY (contest_id) REFERENCES t_contest_info (contest_id),
+    INDEX (username),
+    INDEX (result),
+    INDEX (language)
+);
+
+# 评测结果表
+DROP TABLE IF EXISTS `t_judge_result`;
+CREATE TABLE `t_judge_result`
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    judge_id INT COMMENT '评测ID',
+    info     MEDIUMTEXT NOT NULL COMMENT '评测返回的结果',
+    FOREIGN KEY (judge_id) REFERENCES t_judge_status (id)
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 挑战模式模块表
+DROP TABLE IF EXISTS `t_challenge_block`;
+CREATE TABLE `t_challenge_block`
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(15) NOT NULL COMMENT '模块名称',
+    block_type  INT         NOT NULL COMMENT '模块类型',
+    description TEXT        NOT NULL COMMENT '模块描述'
+);
+
+# 挑战模式题目表
+DROP TABLE IF EXISTS `t_challenge_block_problem`;
+CREATE TABLE `t_challenge_block_problem`
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    block_id      INT NOT NULL COMMENT '挑战模式模块ID',
+    problem_order INT NOT NULL COMMENT '模块内的题目顺序',
+    problem_id    INT NOT NULL COMMENT '题目ID',
+    total_score   INT NOT NULL DEFAULT 0 COMMENT '题目分值',
+    reward_acb    INT NOT NULL DEFAULT 0 COMMENT '解锁后奖励ACB'
+);
+
+# 挑战模式解锁模块条件表
+DROP TABLE IF EXISTS `t_challenge_block_condition`;
+CREATE TABLE `t_challenge_block_condition`
+(
+    id                        INT PRIMARY KEY AUTO_INCREMENT,
+    block_id                  INT NOT NULL COMMENT '挑战模式模块ID',
+    precondition_block_id     INT NOT NULL COMMENT '前置挑战模式模块ID',
+    precondition_unlock_score INT NOT NULL COMMENT '前置模块需要解锁的积分'
+);
+
+# 用户已解锁挑战模块表
+DROP TABLE IF EXISTS `t_challenge_user_open_block`;
+CREATE TABLE `t_challenge_user_open_block`
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    username    VARCHAR(30) NOT NULL,
+    block_id    INT         NOT NULL,
+    unlock_time DATETIME DEFAULT NOW() COMMENT '模块解锁时间'
+);
+
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 讨论帖子表
+DROP TABLE IF EXISTS `t_discuss_post`;
+CREATE TABLE `t_discuss_post`
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(50) NOT NULL,
+    time     DATETIME DEFAULT NOW() COMMENT '帖子建立时间',
+    author   VARCHAR(30) NOT NULL,
+    priority DOUBLE   DEFAULT NULL
+);
+
+# 讨论回复表
+DROP TABLE IF EXISTS `t_discuss_reply_post`;
+CREATE TABLE `t_discuss_reply_post`
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    discuss_id  INT         NOT NULL COMMENT '回帖帖子编号',
+    reply_order INT         NOT NULL COMMENT '回帖楼层顺序',
+    time        DATETIME DEFAULT NOW(),
+    author      VARCHAR(30) NOT NULL,
+    text        TEXT COMMENT '帖子内容'
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# BUG反馈表
+DROP TABLE IF EXISTS `t_bug_report`;
+CREATE TABLE `t_bug_report`
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    username    VARCHAR(30) NOT NULL,
+    title       VARCHAR(255) DEFAULT NULL,
+    type        INT         NOT NULL COMMENT '0：其他 1：系统漏洞 2：功能异常 3：逻辑错误 4：界面问题 ',
+    text        VARCHAR(255) DEFAULT NULL,
+    report_time DATETIME     DEFAULT NOW() COMMENT '反馈时间',
+    is_fixed    SMALLINT     DEFAULT 0,
+    INDEX (username),
+    INDEX (type)
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 用户签到表
+DROP TABLE IF EXISTS `t_user_check_in`;
+CREATE TABLE `t_user_check_in`
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    username   VARCHAR(30) NOT NULL,
+    check_time DATETIME    DEFAULT NOW(),
+    info       VARCHAR(10) DEFAULT 'normal' COMMENT '签到状态',
+    ip_address VARCHAR(20) COMMENT '签到ip地址',
+    INDEX (username)
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# 商城商品表
+DROP TABLE IF EXISTS `t_mall_goods`;
+CREATE TABLE `t_mall_goods`
+(
+    id               INT PRIMARY KEY AUTO_INCREMENT,
+    name             VARCHAR(30) NOT NULL COMMENT '商品名称',
+    cost             INT         NOT NULL DEFAULT 0 COMMENT '所需价格',
+    goods_type       SMALLINT COMMENT '商品大类类型：1.实体物品；2：虚拟物品',
+    stock            INT         NOT NULL DEFAULT -1 COMMENT '库存数量，-1为不限量',
+    description      TEXT        NOT NULL COMMENT '商品介绍',
+    picture_url      VARCHAR(2047) COMMENT '图片url地址',
+    visible          TINYINT              DEFAULT 1 COMMENT '普通用户是否可见：0：不可见；1：可见',
+    shelf_user       VARCHAR(30) COMMENT '上传用户',
+    shelf_time       DATETIME             DEFAULT NOW() COMMENT '上架时间',
+    buy_limit        INT COMMENT '购买限制',
+    buy_verify_limit INT                  DEFAULT 0 COMMENT '购买权限限制：0：登录后不限制；'
+);
+
+# 商城购买订单表
+DROP TABLE IF EXISTS `t_mall_order`;
+CREATE TABLE `t_mall_order`
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    goods_id     INT         NOT NULL,
+    origin_cost  INT         NOT NULL COMMENT '商品初始价格',
+    real_cost    INT         NOT NULL COMMENT '实际付出价格',
+    order_user   VARCHAR(30) NOT NULL COMMENT '购买用户',
+    order_time   DATETIME DEFAULT NOW() COMMENT '下单时间',
+    review_user  VARCHAR(30) COMMENT '审核订单用户',
+    order_status SMALLINT COMMENT '订单状态: 0:无需审核；1：等待审核；2：订单失效；3：订单成交',
+    order_cancel TINYINT  DEFAULT 0 COMMENT '用户是否主动取消：0：未取消订单；1：取消订单'
+);
+
+### ------------------------------------------------------------------------------------------ ###
+
+# ACB变更账单
+DROP TABLE IF EXISTS `t_acb_record`;
+CREATE TABLE `t_acb_record`
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    username   VARCHAR(30) NOT NULL,
+    acb_change INT         NOT NULL,
+    reason     INT         NOT NULL DEFAULT 0 COMMENT 'ACB变更原因：0：默认；',
+    time       DATETIME             DEFAULT NOW() COMMENT '记录时间',
+    mark       VARCHAR(255) COMMENT '原因标记'
+);
+
+### ------------------------------------------------------------------------------------------ ###
